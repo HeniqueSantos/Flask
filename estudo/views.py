2a3664169e0350949198c310a422c09023b9bec5
HeniqueSantos <henriquedosantos206@gmail.com>
2026-02-06 16:33:46 -0300

    melhoria no teste e descobrindo mais coisas

--- estudo/views.py
+++ estudo/views.py
@@ -1,6 +1,25 @@
 from estudo import app
-from flask import render_template
+from flask import render_template, url_for
+
 @app.route('/')
 
 def homepage():
-    return render_template('index.html')
+
+    context = {
+        'usuario1': {
+             'nome': 'Henrique',
+             'sobrenome': 'Dos Santos',
+             'Gmail': 'seugmail@gmail.com',
+             'Senha': '123456'
+        },
+        'usuario2': {}
+    }
+    return render_template('index.html', context=context)
+
+@app.route('/bem-vindos')
+def bem_vindos():
+    return render_template('bem-vindos.html')
+
+@app.route('/contatos')
+def contatos():
+    return render_template('contatos.html')
